31035d0f9ff89eec389825a0d9c7e5716cac349d
Bata Zoltán <47896536+bzozoo@users.noreply.github.com>
2022-05-22 19:31:40 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+const endpoint = "https://zrivv1.sse.codesandbox.io/"
 
+async function getDatas(){
+  try {
+    const response = await fetch(endpoint)
+    return response.json()
+  }catch(e){
+    console.log(e);
+    return false;
+  }
+}
+
+getDatas()
